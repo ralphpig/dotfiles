4eae3d95703514560876b134e932ee0a1cfd694e
Mike <mail@ralphpig.dev>
2024-12-04 22:01:20 -0500

    chore(nix): neovim config

--- home.nix
+++ home.nix
@@ -1,17 +1,14 @@
-{ lib, config, pkgs, ... }:
-
-with lib.hm.gvariant;
 {
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib.hm.gvariant; {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "ralphpig";
   home.homeDirectory = "/home/ralphpig";
-
-  programs.git = {
-    enable = true;
-    userEmail = "mail@ralphpig.dev";
-    userName = "Mike";
-  };
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -78,132 +75,156 @@ with lib.hm.gvariant;
   #  /etc/profiles/per-user/ralphpig/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  # Programs
+
+  programs.git = {
+    enable = true;
+    userEmail = "mail@ralphpig.dev";
+    userName = "Mike";
+    extraConfig = {
+      safe.directory = "*";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    extraConfig = ''
+      set shiftwidth=2 smarttab
+      set expandtab
+      set tabstop=8 softtabstop=0
+    '';
+  };
+
   programs.zsh = {
-  	enable = true;
-  	enableCompletion = true;
-  	autosuggestion.enable = true;
-  	syntaxHighlighting.enable = true;
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
 
-	history = {
-    		size = 10000;
-  	};
+    history = {
+      size = 10000;
+    };
 
-	oh-my-zsh = {
-  		enable = true;
-    		plugins = [
-			"git"
-		];
-    		theme = "ys";
-  	};
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+      ];
+      theme = "ys";
+    };
+
+    shellAliases = {
+      cpwd = "pwd | tee >(wl-copy)";
+    };
   };
 
   # dconf
-  dconf.settings =  {
-	# GNOME
-	"org/gnome/desktop/interface" = {
-		color-scheme = "prefer-dark";
-		clock-show-weekday = true;
-		clock-show-seconds = true;
-		clock-format = "12h";
-	};
+  dconf.settings = {
+    # GNOME
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      clock-show-weekday = true;
+      clock-show-seconds = true;
+      clock-format = "12h";
+    };
 
-	"org/gnome/mutter" = {
-		workspaces-only-on-primary = true;
-	};
+    "org/gnome/mutter" = {
+      workspaces-only-on-primary = true;
+    };
 
-	"org/gnome/desktop/wm/preferences" = {
-		num-workspaces = 10;
-	};
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 10;
+    };
 
-	"org/gnome/desktop/input-sources" = {
-		xkb-options = ["altwin:swap_alt_win"]; # Swap Super to Alt
-	};
+    "org/gnome/desktop/input-sources" = {
+      xkb-options = ["altwin:swap_alt_win"]; # Swap Super to Alt
+    };
 
-	"org/gnome/desktop/datetime" = {
-		automatic-timezone = true;
-	};
-   	"org/gnome/shell" = {
-		last-selected-power-profile = "performance";
-		favorite-apps = [
-  			"org.gnome.Nautilus.desktop"
-			"org.gnome.Console.desktop"
-			"microsoft-edge.desktop"
-			"sublime_text.desktop"
-			"sublime_merge.desktop"
-		];
+    "org/gnome/desktop/datetime" = {
+      automatic-timezone = true;
+    };
+    "org/gnome/shell" = {
+      last-selected-power-profile = "performance";
+      favorite-apps = [
+        "org.gnome.Nautilus.desktop"
+        "org.gnome.Console.desktop"
+        "microsoft-edge.desktop"
+        "sublime_text.desktop"
+        "sublime_merge.desktop"
+      ];
 
-		
-		# Extensions
-		disable-user-extensions = false;
-		enabled-extensions = [
-			"places-menu@gnome-shell-extensions.gcampax.github.com"
-			"drive-menu@gnome-shell-extensions.gcampax.github.com"
-			"system-monitor@gnome-shell-extensions.gcampax.github.com"
-			"workspace-indicator@gnome-shell-extensions.gcampax.github.com"
-			"windowsNavigator@gnome-shell-extensions.gcampax.github.com"
-      		];
-	};
+      # Extensions
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "system-monitor@gnome-shell-extensions.gcampax.github.com"
+        "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+        "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+      ];
+    };
 
-	# Keyboard
-	"org/gnome/desktop/peripherals/keyboard" = {
-		delay = (mkUint32 200);
-		repeat-interval = (mkUint32 30);
-	};
+    # Keyboard
+    "org/gnome/desktop/peripherals/keyboard" = {
+      delay = mkUint32 200;
+      repeat-interval = mkUint32 30;
+    };
 
-	# Keybindings
-	"org/gnome/desktop/wm/keybindings" = {
-		switch-to-workspace-left = ["<Super>a"];
-		switch-to-workspace-right = ["<Super>d"];
-		switch-to-workspace-1 = ["<Super>1"];
-		switch-to-workspace-2 = ["<Super>2"];
-		switch-to-workspace-3 = ["<Super>3"];
-		switch-to-workspace-4 = ["<Super>4"];
-		switch-to-workspace-5 = ["<Super>5"];
-		switch-to-workspace-6 = ["<Super>6"];
-		switch-to-workspace-7 = ["<Super>7"];
-		switch-to-workspace-8 = ["<Super>8"];
-		switch-to-workspace-9 = ["<Super>9"];
+    # Keybindings
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-left = ["<Super>a"];
+      switch-to-workspace-right = ["<Super>d"];
+      switch-to-workspace-1 = ["<Super>1"];
+      switch-to-workspace-2 = ["<Super>2"];
+      switch-to-workspace-3 = ["<Super>3"];
+      switch-to-workspace-4 = ["<Super>4"];
+      switch-to-workspace-5 = ["<Super>5"];
+      switch-to-workspace-6 = ["<Super>6"];
+      switch-to-workspace-7 = ["<Super>7"];
+      switch-to-workspace-8 = ["<Super>8"];
+      switch-to-workspace-9 = ["<Super>9"];
 
-		close = ["<Shift><Super>q"];
-	};
-	"org/gnome/shell/keybindings" = {
-		switch-to-application-1 = [];
-		switch-to-application-2 = [];
-		switch-to-application-3 = [];
-		switch-to-application-4 = [];
-		switch-to-application-5 = [];
-		switch-to-application-6 = [];
-		switch-to-application-7 = [];
-		switch-to-application-8 = [];
-		switch-to-application-9 = [];
-	};
+      close = ["<Shift><Super>q"];
+    };
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+    };
 
-	# Custom Keybinds
-	"org/gnome/settings-daemon/plugins/media-keys" = {
-		custom-keybindings = [
-			"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-		];
-	};
-	"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-		name = "Open Terminal";
-		binding = "<Super>Return";
-		command = "kgx";
-	};
+    # Custom Keybinds
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Open Terminal";
+      binding = "<Super>Return";
+      command = "kgx";
+    };
 
-	"org/gnome/shell/extensions/system-monitor" = {
-		show-upload = false;
-		show-download = false;
-	};
+    "org/gnome/shell/extensions/system-monitor" = {
+      show-upload = false;
+      show-download = false;
+    };
 
-	# GTK
-	"org/gtk/settings/file-chooser" = {
-		clock-format = "12h";
-	};
+    # GTK
+    "org/gtk/settings/file-chooser" = {
+      clock-format = "12h";
+    };
   };
 }
